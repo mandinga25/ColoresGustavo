a3280aabd9021e854aaff0c37966c126bff9a48a
Your-Full-Name <your-email-address>
2019-11-19 20:43:23 -0300

    Cambio de colores favoritos

--- src/coloresgustavo/ColoresGustavo.java
+++ src/coloresgustavo/ColoresGustavo.java
@@ -7,9 +7,9 @@ public class ColoresGustavo {
    
     public static void main(String[] args) {
         String color1, color2, color3;
-        color1="AZUL";
+        color1="AMARILLO";
         color2="ROJO";
-        color3="VERDE";
+        color3="VIOLETA";
         System.out.printf("%s es mi primer color favorito\n", color1);
         System.out.printf("%s es mi segundo color favorito\n", color2);
         System.out.printf("%s es mi tercer color favorito\n", color3);
