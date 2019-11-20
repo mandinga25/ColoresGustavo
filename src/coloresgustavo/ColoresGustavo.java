1bafc57be39e71c821162ac0dc44c89f1d07b0ea
mandinga25 <gusta.king.25@gmail.com>
2019-11-19 21:00:40 -0300

    Se amplia la lista a cuatro colores

--- src/coloresgustavo/ColoresGustavo.java
+++ src/coloresgustavo/ColoresGustavo.java
@@ -12,7 +12,7 @@ public class ColoresGustavo {
         color3="PURPURA";
         color4="MAGENTA";
         System.out.printf("%s es mi primer color favorito\n", color1);
-        System.out.printf("%s es mi segundo color favorito\n", color2);
+        System.out.printf("%s no es mi color principañ pero tambien me gusta\n", color2);
         System.out.printf("%s es mi tercer color favorito\n", color3);
         System.out.printf("%s es el ultimo color de la lista\n", color4);
     }
